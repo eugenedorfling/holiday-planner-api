dc5edbf2be77655257535b3c48ae78ee3ece4a6b
Eugene Dorfling <eldorfling@gmail.com>
2024-10-21 15:12:31 +0200

    refactor(core): Revert Django Secret Settings
    - Using default clear secret for simplicity in development

--- core/settings.py
+++ core/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# Moved security key to env file
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-il&%en!#a7*p)kf3)yzi8*g!fpzkpk5)k--vxea*0zr=b&igyj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

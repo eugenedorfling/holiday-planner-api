6ef380a0a0a87de0d04a5b36e39672190ce7e9bb
Eugene Dorfling <eldorfling@gmail.com>
2024-10-21 23:18:18 +0200

    feat(admin): register models

--- holiday_planner/admin.py
+++ holiday_planner/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from holiday_planner.models import HolidaySchedule, Destination, ScheduleItem
 
 # Register your models here.
+
+admin.site.register(HolidaySchedule)
+admin.site.register(Destination)
+admin.site.register(ScheduleItem)
